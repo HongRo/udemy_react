e38d348099375e5fa69446183d3c819e6dd0d64c
Robert Hong <robert.hong@capitalone.com>
2021-08-24 15:33:07 -0400

    first app

---
+++ my-app/src/index.js
@@ -0,0 +1,23 @@
+// How to get stuff to show on screen with React
+// import react and react dom libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// create a react component
+const App = () => {
+  const buttonText = { text: "click me" };
+  const style = { backgroundColor: "blue", color: "white" };
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>{buttonText.text}</button>
+    </div>
+  );
+};
+
+// take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
